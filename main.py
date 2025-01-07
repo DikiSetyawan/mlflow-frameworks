a6f31f5457bc1bc80a285558ad4fdb3555af53f5
Diki <dikisetyawan.ds24@gmail.com>
2025-01-07 17:47:35 +0700

    new update

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     model_uri =flexible_training(df, test_data_path)
     print(model_uri)
     #model_uri = 'runs:/533e369df5164901a580e5ec0b375d1f/Linear_regression_model_awal'
+    monitor_model(model_uri)
+    #mlflow.sklearn.load_model(model_uri)
 
-    mlflow.sklearn.load_model(model_uri)
     #monitor_model("Linear_regression_model_awal")
 

011da13f0c5285a4a6cc695c83c1c445d48950b1
Diki <dikisetyawan.ds24@gmail.com>
2025-01-08 11:09:13 +0700

    update training xgboost

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 mlflow.set_tracking_uri("http://localhost:5000")
 
-mlflow.autolog()
+#mlflow.autolog()
 
 
 mappings = {
@@ -126,40 +126,41 @@ def training_xg(
         max_depth = 3,
         learning_rate = 0.1,
         run_name = f"Xgboost_regression{get_current_time()}",
-        fit_intercept = True, 
-        n_jobs = 2
 ):
-    
+    # Disable autologging
+    mlflow.autolog(disable=True)
+
     mlflow.set_experiment("testingMlflow")
     train_df = load_and_preprocess('/home/dikidwidasa/mlflow/data/train.csv')
     test_df = load_and_preprocess('/home/dikidwidasa/mlflow/data/test.csv')
     X_train, y_train = splitting_data(train_df)
     X_test, y_test = splitting_data(test_df)
-    with mlflow.start_run(run_name=run_name) as run : xgb_model = xgb.XGBRegressor(
-        n_estimators = n_estimators,
-        max_depth = max_depth,
-        learning_rate = learning_rate
-    )
-
-    xgb_model.fit(X_train, y_train)
-    y_pred = xgb_model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    mlflow.log_metric('mse', mse)
-    mlflow.log_metric('r2', r2)
-
-    mlflow.xgboost.log_model(xgb_model, model_name)
-    run_id = run.info.run_id
     
-    model_uri = f"runs:/{run_id}/{model_name}"
-    registered_model_version = mlflow.register_model(model_uri, model_name)
+    with mlflow.start_run(run_name=run_name) as run:
+        xgb_model = xgb.XGBRegressor(
+            n_estimators = n_estimators,
+            max_depth = max_depth,
+            learning_rate = learning_rate
+        )
+        xgb_model.fit(X_train, y_train)
+        y_pred = xgb_model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
 
-    print(f"Model registered: {model_name}, version: {registered_model_version.version}")
-    result = {
-        'model_uri' : model_uri, 
-        'registered_model_name' : model_name, 
-        'registered_model_version' : registered_model_version,
-        'run_id' : run_id
-    } 
+        mlflow.log_metric('mse', mse)
+        mlflow.log_metric('r2', r2)
+        mlflow.xgboost.log_model(xgb_model, model_name)
+        run_id = run.info.run_id
+
+        model_uri = f"runs:/{run_id}/{model_name}"
+        registered_model_version = mlflow.register_model(model_uri, model_name)
+
+        print(f"Model registered: {model_name}, version: {registered_model_version.version}")
+        result = {
+            'model_uri' : model_uri, 
+            'registered_model_name' : model_name, 
+            'registered_model_version' : registered_model_version,
+            'run_id' : run_id
+        } 
     return result
 
